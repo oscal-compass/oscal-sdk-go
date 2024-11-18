e00801d8042179b3cb1a625d3b32e133b079a770
Jennifer Power <barnabei.jennifer@gmail.com>
2024-11-18 08:54:02 -0500

    docs: refines public symbol comments for clarity
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- extensions/rules.go
+++ extensions/rules.go
@@ -2,7 +2,7 @@
 
 package extensions
 
-// Property names for trestle-based extensions
+// Below are defined oscal.Property names for compass-based extensions
 const (
 	RuleIdProp               = "Rule_Id"
 	RuleDescriptionProp      = "Rule_Description"
@@ -13,36 +13,40 @@ const (
 	ParameterDefaultProp     = "Parameter_Value_Default"
 )
 
+// RuleSet defines a Rule instance with associated
+// Check implementation data.
 type RuleSet struct {
-	// A single instance of a rule
+	// Rule is a single rule instance associated with the set.
 	Rule Rule
-	// A set of automation information called "checks"
+	// Checks include all associated check data registered for the rule.
 	Checks []Check
 }
 
-// Rule defines a single rule with all associated metadata
+// Rule defines a single compliance rule which can also be defined
+// as a technical control or a way to validate implemented requirements.
 type Rule struct {
-	// Rule identification
+	// ID is a string representation of the rule identifier.
 	ID string
-	// High level description
-	RuleDescription string
-	// Associated rule parameter information for tuning options.
+	// Description defines description of what the rule does.
+	Description string
+	// Parameter is optional information for tuning rule logic.
 	Parameter *Parameter
 }
 
-// Check defines a single check with all associated metadata.
+// Check defines a single concrete implementation of a Rule.
 type Check struct {
-	// Associated check implementation identification
+	// ID is a string representation of the check identifier.
 	ID string
-	// High level check description
+	// Description defines description of what the check does.
 	Description string
 }
 
-// Parameter identifies a parameter or variable that can be used to alter rule logic
+// Parameter identifies a parameter or variable that can be used to alter rule logic.
 type Parameter struct {
-	// Parameter Identification
-	ID          string
+	// ID is a string representation of the parameter identifier.
+	ID string
+	// Description defines description of what the parameter does.
 	Description string
-	// The selected value for the parameter
+	// Value is the selected value or option for the parameter.
 	Value string
 }
--- generators/loader.go
+++ generators/loader.go
@@ -9,7 +9,7 @@ import (
 	oscal112 "github.com/defenseunicorns/go-oscal/src/types/oscal-1-1-2"
 )
 
-// NewCatalog creates a new OSCAL-based control catalog using types from `go-oscal`
+// NewCatalog creates a new OSCAL-based control catalog using types from `go-oscal`.
 func NewCatalog(reader io.Reader) (catalog *oscal112.Catalog, err error) {
 	var oscalModels oscal112.OscalModels
 	dec := json.NewDecoder(reader)
@@ -21,7 +21,7 @@ func NewCatalog(reader io.Reader) (catalog *oscal112.Catalog, err error) {
 	return oscalModels.Catalog, nil
 }
 
-// NewProfile creates a new OSCAL-based profile using types from `go-oscal`
+// NewProfile creates a new OSCAL-based profile using types from `go-oscal`.
 func NewProfile(reader io.Reader) (profile *oscal112.Profile, err error) {
 	var oscalModels oscal112.OscalModels
 	dec := json.NewDecoder(reader)
@@ -33,7 +33,7 @@ func NewProfile(reader io.Reader) (profile *oscal112.Profile, err error) {
 	return oscalModels.Profile, nil
 }
 
-// NewComponentDefinition creates a new OSCAL-based component definition using types from `go-oscal`
+// NewComponentDefinition creates a new OSCAL-based component definition using types from `go-oscal`.
 func NewComponentDefinition(reader io.Reader) (componentDefinition *oscal112.ComponentDefinition, err error) {
 	var oscalModels oscal112.OscalModels
 	dec := json.NewDecoder(reader)
@@ -45,7 +45,7 @@ func NewComponentDefinition(reader io.Reader) (componentDefinition *oscal112.Com
 	return oscalModels.ComponentDefinition, nil
 }
 
-// NewSystemSecurityPlan creates a new OSCAL-based system security plan using types from `go-oscal`
+// NewSystemSecurityPlan creates a new OSCAL-based system security plan using types from `go-oscal`.
 func NewSystemSecurityPlan(reader io.Reader) (systemSecurityPlan *oscal112.SystemSecurityPlan, err error) {
 	var oscalModels oscal112.OscalModels
 	dec := json.NewDecoder(reader)
@@ -57,7 +57,7 @@ func NewSystemSecurityPlan(reader io.Reader) (systemSecurityPlan *oscal112.Syste
 	return oscalModels.SystemSecurityPlan, nil
 }
 
-// NewAssessmentPlan creates a new OSCAL-based assessment plan using types from `go-oscal`
+// NewAssessmentPlan creates a new OSCAL-based assessment plan using types from `go-oscal`.
 func NewAssessmentPlan(reader io.Reader) (assessmentPlan *oscal112.AssessmentPlan, err error) {
 	var oscalModels oscal112.OscalModels
 	dec := json.NewDecoder(reader)
@@ -69,7 +69,7 @@ func NewAssessmentPlan(reader io.Reader) (assessmentPlan *oscal112.AssessmentPla
 	return oscalModels.AssessmentPlan, nil
 }
 
-// NewAssessmentResults creates a new OSCAL-based assessment results set using types from `go-oscal`
+// NewAssessmentResults creates a new OSCAL-based assessment results set using types from `go-oscal`.
 func NewAssessmentResults(reader io.Reader) (assessmentResult *oscal112.AssessmentResults, err error) {
 	var oscalModels oscal112.OscalModels
 	dec := json.NewDecoder(reader)
@@ -81,7 +81,7 @@ func NewAssessmentResults(reader io.Reader) (assessmentResult *oscal112.Assessme
 	return oscalModels.AssessmentResults, nil
 }
 
-// NewPOAM creates a new OSCAL-based plan of action and milestones using types from `go-oscal`
+// NewPOAM creates a new OSCAL-based plan of action and milestones using types from `go-oscal`.
 func NewPOAM(reader io.Reader) (pOAM *oscal112.PlanOfActionAndMilestones, err error) {
 	var oscalModels oscal112.OscalModels
 	dec := json.NewDecoder(reader)
