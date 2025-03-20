586a1cfee4ffca0ad149bd190dff13cf7e6b4246
Jennifer Power <barnabei.jennifer@gmail.com>
2025-03-20 18:51:36 -0400

    fix: updates typos in doc comments and error strings
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- models/components/doc.go
+++ models/components/doc.go
@@ -4,5 +4,5 @@
 */
 
 // Package components defines logic for working with different defined OSCAL component types.
-// This provides a generic implementations of Components and associated Control Implementations.
+// This provides a generic implementation of Components and associated Control Implementations.
 package components
