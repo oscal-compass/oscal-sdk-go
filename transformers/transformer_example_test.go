351cc83697ba163c8e1aecf0cf8ab3844722047b
d10n <d10n@redhat.com>
2025-05-21 17:52:45 -0400

    chore: move example from readme to godocs (#70)
    * chore: move example from readme to godocs
    
    Fixes #66
    
    Signed-off-by: d10n <d10n@redhat.com>
    
    * chore: fix example output
    
    Signed-off-by: d10n <d10n@redhat.com>
    
    ---------
    
    Signed-off-by: d10n <d10n@redhat.com>
    Co-authored-by: Jennifer Power <barnabei.jennifer@gmail.com>

---
+++ transformers/transformer_example_test.go
@@ -0,0 +1,41 @@
+package transformers_test
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	oscalTypes "github.com/defenseunicorns/go-oscal/src/types/oscal-1-1-3"
+
+	"github.com/oscal-compass/oscal-sdk-go/validation"
+
+	"github.com/oscal-compass/oscal-sdk-go/models"
+	"github.com/oscal-compass/oscal-sdk-go/transformers"
+)
+
+func ExampleComponentDefinitionsToAssessmentPlan() {
+	file, err := os.Open("../testdata/component-definition-test.json")
+	if err != nil {
+		log.Fatalf("failed to open component definition, %v", err)
+	}
+	definition, err := models.NewComponentDefinition(file, validation.NoopValidator{})
+	if err != nil {
+		log.Fatalf("failed to read component definition, %v", err)
+	}
+
+	if definition != nil {
+		assessmentPlan, err := transformers.ComponentDefinitionsToAssessmentPlan(context.Background(), []oscalTypes.ComponentDefinition{*definition}, "cis")
+		if err != nil {
+			log.Fatalf("failed to create assessment plan, %v", err)
+		}
+		reviewedControlsJson, err := json.Marshal(assessmentPlan.ReviewedControls)
+		if err != nil {
+			log.Fatalf("failed to marshal reviewed controls, %v", err)
+		}
+		fmt.Println(string(reviewedControlsJson))
+	}
+	// Output:
+	// {"control-selections":[{"include-controls":[{"control-id":"CIS-2.1"}]}]}
+}
